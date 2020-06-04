7026405bff964112aed29fad59d15d6ec3bd82e2
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-06-04 07:02:17 +0530

    navbar minor updates

--- src/app/component/utility/navbar/navbar.component.ts
+++ src/app/component/utility/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../../../service/login.service';
-import * as jwt_decode from 'jwt-decode';
+import { Router } from '@angular/router';
 @Component({
     selector: 'app-navbar',
     templateUrl: './navbar.component.html',
@@ -8,18 +8,8 @@ import * as jwt_decode from 'jwt-decode';
 })
 export class NavbarComponent implements OnInit {
     show_navbar: boolean = false;
-    public admin: Boolean = false;
-    public intern: boolean = false;
-    constructor(private _auth: LoginService) {
-        if (this._auth.loggedIn()) {
-            if (jwt_decode(this._auth.getToken()).admin) this.admin = true;
-            else this.intern = true;
-        }
-    }
-    ngOnInit() { }
-    logout(): void {
-        localStorage.removeItem('token');
-        this.admin = false;
-        this.intern = false;
+    constructor(private _auth: LoginService, private _router: Router) { }
+    ngOnInit() {
+        if (this._auth.loggedIn()) this._router.navigate(['/usip/intern']);
     }
 }
